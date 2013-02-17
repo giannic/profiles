ae5b9ad17e5cb32b09845db83641ed4a4423d6ef
Kevin Shen <kshen@seas.upenn.edu>
2013-02-17 18:02:35 -0500

    Added variance to data

--- scripts/generatedata.py
+++ scripts/generatedata.py
@@ -15,9 +15,78 @@ def main():
              "Entertainment2", "Entertainment3", "Entertainment4",
              "Entertainment5"]
 
-    categories = ['Social Networks', 'Professional', 'Entertainment']
 
     data = {site: generate_times() for site in sites}
+    data = add_categories(data)
+
+    '''
+    for site in data:
+        print "%s: %d" % (site, len(data[site]["open"]))
+    '''
+
+    data = add_variance(data)
+
+    '''
+    print "\n\nafter variance"
+
+    for site in data:
+        print "%s: %d" % (site, len(data[site]["open"]))
+    '''
+
+    # print_stuff(data)
+
+    with open('usage_data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+def add_variance(data):
+    '''Makes some apps less frequently used.
+    Removes some intermittent and last few sessions.
+    This should result in shorter duration, with a longer "last used" date'''
+    #keys = data.keys()
+    # for i in xrange(0, len(keys), 3):
+
+    # for every site
+    for site in data:
+        app_data = data[site]
+        # remove every nth element
+        n = random.randint(3, 20)
+        new_open = []
+        new_close = []
+        old_open = app_data["open"]
+        old_close = app_data["close"]
+        for j in xrange(len(old_open)):
+            if j % n != 0:
+                new_open.append(old_open[j])
+                new_close.append(old_close[j])
+
+        # remove last k elements to simulate apps you no longer use
+        k = random.randint(1, 100)
+        for _ in xrange(k):
+            new_open.pop()
+            new_close.pop()
+
+        app_data["open"] = new_open
+        app_data["close"] = new_close
+
+    return data
+
+
+
+
+
+
+def print_stuff(data):
+    # prints the number of visits for Facebook
+    print len(data["Facebook"]["open"])
+    print len(data["Facebook"]["close"])
+
+    # prints the duration of each visit to Facebook in H:M:S
+    for start, end in zip(data["Facebook"]["open"], data["Facebook"]["close"]):
+        print str(timedelta(seconds=end - start))
+
+def add_categories(data):
+    categories = ['Social Networks', 'Professional', 'Entertainment']
+
     data['Facebook']['category'] = categories[0]
     data['Twitter']['category'] = categories[0]
     data['LinkedIn']['category'] = categories[0]
@@ -38,20 +107,7 @@ def main():
     data['Entertainment4']['category'] = categories[0]
     data['Entertainment5']['category'] = categories[0]
 
-    # prints the number of visits for Facebook
-    '''
-    print len(data["Facebook"]["open"])
-    print len(data["Facebook"]["close"])
-    '''
-
-    # prints the duration of each visit to Facebook in H:M:S
-    '''
-    for start, end in zip(data["Facebook"]["open"], data["Facebook"]["close"]):
-        print str(timedelta(seconds=end - start))
-    '''
-
-    with open('usage_data.json', 'w') as outfile:
-        json.dump(data, outfile)
+    return data
 
 
 def generate_times():
