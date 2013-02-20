a4ec09ce0d8f36444101730f2f7b999fec19c329
Gianni Chen <giannikchen@gmail.com>
2013-02-20 03:30:48 -0500

    very simple randomized animation

--- js/floats.js
+++ js/floats.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 var CENTER = 250;
-var theta, x, y, duration;
+var theta, x, y, duration, current;
 
 var jsonStats = loadJSON();
 var usageStats = getDurationAndLastVisitStats(jsonStats);
@@ -19,13 +19,30 @@ for (var entry in usageStats) {
     y = Math.sin(theta);
 
     duration = usageStats[entry].duration
-    svgcontext.append("circle")
-              .attr("cx", CENTER + duration*0.00003*x)
-              .attr("cy", CENTER + duration*0.00003*y)
-              .attr("r", duration*0.000005)
-              .attr("stroke", "black")
-              .attr("fill", "steelblue")
-              .attr("opacity", duration*0.0000001);
+    current = svgcontext.append("circle")
+                        .attr("cx", CENTER + duration*0.00003*x)
+                        .attr("cy", CENTER + duration*0.00003*y)
+                        .attr("r", duration*0.000005)
+                        .attr("stroke", "black")
+                        .attr("fill", "steelblue")
+                        .attr("opacity", duration*0.0000001);
+    if (usageStats[entry].active === 0) {
+        current.transition()
+               .duration(10000)
+               .ease(Math.sqrt) // absolutely terrible formulas here
+               // what it is, twice the radius from the center compared to
+               // when it was initialized, will change later
+               .attr("cx", CENTER + duration*0.00003*x*2)
+               .attr("cy", CENTER + duration*0.00003*y*2)
+               .attr("opacity", duration*0.00000005);
+    } else if (usageStats[entry].active === 1) {
+        current.transition()
+               .duration(10000)
+               .ease(Math.sqrt) // absolutely terrible formulas here also
+               .attr("cx", CENTER + duration*0.00003*x*0.5)
+               .attr("cy", CENTER + duration*0.00003*y*0.5)
+               .attr("r", duration*0.00001);
+    }
 }
 
 function getDurationAndLastVisitStats(jsonStats) {
@@ -38,7 +55,10 @@ function getDurationAndLastVisitStats(jsonStats) {
             duration += closeList[i] - openList[i];
         }
         output[site] = {"duration": duration,
-                        "last_visit": closeList[closeList.length - 1]};
+                        "last_visit": closeList[closeList.length - 1],
+                        "active": Math.floor(Math.random()*2)};
+                        // just randomly generating on frontend for now
+                        // need to come from extension
     }
     return output;
 }
