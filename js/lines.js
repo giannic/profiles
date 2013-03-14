a5b1eac8bb280b06226c8e62970fd473f1930738
samanthamerritt <merritts@seas.upenn.edu>
2013-03-14 04:33:31 -0400

    edited lines.js to allow for user input and ranges, and modified timeline.html, but same code

--- js/lines.js
+++ js/lines.js
@@ -1,13 +1,12 @@
-$(document).ready(function() {
 var numberOfLines,
     stats = null,
     openArray, renderArray, closeArray,
     startTime, endTime, difference,
     lineGraphWidth, lineGraphHeight, lineGraph,
-    allTheLines, hsl,
+    allTheLines, hsl, colorArray,
     myVar; // think this is for timing later?
 
-numberOfLines = 1300;
+$(document).ready(function() {
 
 $.ajaxSetup({
     "async": false
@@ -18,38 +17,43 @@ $.getJSON('usage_data.json', function(data) {
     console.log('loaded');
 });
 
+// put the stage dimensions here
+lineGraphWidth = 1000;
+lineGraphHeight = 600;
+
+//make this section dynamic.
 openArray = stats['Spotify']['open'];
 renderArray = stats['Spotify']['open'];
 closeArray = stats['Spotify']['close'];
+
+
+numberOfLines = openArray.length;
 
 // start and end times and difference
 startTime = openArray[0];
 endTime = closeArray[closeArray.length-1];
 difference = endTime - startTime;
 
-// put the stage dimensions here
-lineGraphWidth = 1300;
-lineGraphHeight = 600;
-
-function calculateRender(startVal) {
-    var i;
-    startTime = startVal;
-    difference = endTime - startTime;
-    for (i = 0; i < openArray.length; i++) {
-        renderArray[i] = ((openArray[i]-startTime)/(difference/lineGraphWidth));
-    }
-}
-
-calculateRender(openArray[numberOfLines]);
+//here's where to put Gabys input
+//calculateRender(1100, 1140);
 
 // array to store the lines
 allTheLines = [];
 
-// rgb_hsl order to show dynamic color shift
-hsl = 0;
+// Select the DIV container "D3line" and add an SVG element to it
+lineGraph = d3.select("#D3line")
+              .append("svg:svg")
+              .attr("width", lineGraphWidth)
+              .attr("height", lineGraphHeight);
+
+//myVar = setInterval(function(){myTimer()},1000);
+
+ });
 
 function generateLines(currArray) {
     var currentLine, i;
+    d3.selectAll("line").remove();
+    allTheLines =  [];
     for (i = 0; i < currArray.length; i++) {
         currentLine = lineGraph.append("svg:line")
                                .attr("x1", currArray[i])
@@ -58,36 +62,37 @@ function generateLines(currArray) {
                                .attr("y2", lineGraphHeight)
                                .attr("name", "line"+i)
                                .style("stroke", "rgb(255,255,255)")
-                               .style("stroke-width", 2);
+                               .style("stroke-width", 2)
+                               .style("stroke", "hsl(200,50%, 50%)");
 
         allTheLines[i] = currentLine;
     }
 }
 
-// Select the DIV container "D3line" and add an SVG element to it
-lineGraph = d3.select("#D3line")
-              .append("svg:svg")
-              .attr("width", lineGraphWidth)
-              .attr("height", lineGraphHeight);
-
 function changeColor(lineArray, opacityOfLine) {
     var i;
     for (i = 0; i<lineArray.length; i++) {
         lineArray[i].style("stroke-opacity", opacityOfLine);
-        lineArray[i].style("stroke", "hsl("+hsl+",50%, 50%)");
-        if (hsl > 360) {
-            hsl = 0;
-        } else {
-            hsl += 1.0;
-        }
     }
 }
 
-// sparklines mouseclick event
+function calculateRender(startValIndex, endValIndex) {
+    startTime = openArray[startValIndex];
+    endTime = closeArray[endValIndex];
+    var i;
+    difference = endTime - startTime;
+    renderArray = [];
+    for (i = 0; i < openArray.length; i++) {
+        renderArray[i] = ((openArray[i]-startTime)/(difference/lineGraphWidth));
+    }
+    generateLines(renderArray);
+    console.log(startValIndex);
+    console.log(endValIndex);
+    changeColor(allTheLines, 1.0);
+}
 
-//myVar = setInterval(function(){myTimer()},1000);
-
-generateLines(renderArray);
-changeColor(allTheLines, 1.0);
-
-});
+function getInfo(){
+    var range1 = prompt("enter a number between 1 and 1368");
+    var range2 = prompt("enter a number between " + range1 + " and 1369");
+    calculateRender(range1, range2);
+ }
