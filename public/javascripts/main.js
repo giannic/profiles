056e55b31975aadf6800fe696a9e9850dddd1fcd
David Xu <xudavidb@gmail.com>
2013-03-15 01:29:24 -0400

    FIXED THAT SHIEEEEEEEEET

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,50 +1,57 @@
 $('document').ready(function() {
-    $('#circles > svg').css({
-        "position": "absolute",
-        "left": 0,
-        "top": 60
-    })
-        .attr("width", "800px")
-        .attr("height", "900px");
+    $('#visualizations').width(2*$(window).width() + 5 + 'px');
+    $('#circles').width($(window).width() + 'px');
+    $('#lines').width($(window).width() + 'px');
+  
+  
+  // css({
+  //       // "position": "absolute",
+  //       // "left": 0,
+  //       // "top": 60
+  //   })
+  //       .attr("width", $(window).width())
+  //       .attr("height", $(window).width());
 
 
     $('#D3line > svg').css({
-        "position": "absolute",
-        "left": $(window).width(),
-        "top": 60
+        // "position": "absolute",
+        // "left": $(window).width(),
+        // "top": 60
     });
 
     $('#timeline_panel').css({
-        "left": $(window).width(),
-        "top": $(window).width()
+        // "left": $(window).width(),
+        // "top": $(window).width()
     });
 
     $('#timelines-toggle').click(function() {
-        $('#timeline_panel').css({"background-color":"black"});
-        $('svg').animate({
+      console.log('MOVEEEEEE')
+        // $('#timeline_panel').css({"background-color":"black"});
+        $('#visualizations').animate({
             left: '-=' + $(window).width()
         }, 300, function() {
         });
 
-        $('#timeline_panel').animate({
-            left: '-=' + $(window).width()
-        }, 300, function() {
+        // $('#timeline_panel').animate({
+        //     left: '-=' + $(window).width()
+        // }, 300, function() {
 
-        });
+        // });
 
     });
 
     $('#clusters-toggle').click(function() {
-        $('svg').animate({
+      console.log('animate')
+        $('#visualizations').animate({
             left: '+=' + $(window).width()
         }, 300, function() {
 
         });
 
-        $('#timeline_panel').animate({
-            left: '+=' + $(window).width()
-        }, 300, function() {
+        // $('#timeline_panel').animate({
+        //     left: '+=' + $(window).width()
+        // }, 300, function() {
 
-        });
+        // });
     });
 });
