edfe4ba6e34f5b75a351efc903e6627293965add
Fannie <fanniefliu@gmail.com>
2013-03-28 16:53:00 -0400

    overlap fixed, mouseenter/mouseleave support added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,8 +4,10 @@ $('document').ready(function() {
     $('#lines').width($(window).width() + 'px');
 
     $('#timelines-toggle').click(function() {
+        console.log("timelines")
         $('#visualizations').animate({
             // we should probably have -1 * vis_number * width (to scale)
+            
             left: -$(window).width()
         }, 300, function() {
         });
@@ -24,4 +26,48 @@ $('document').ready(function() {
         console.log("adding account");
         $("#add-app-box").toggle();
     });
+    
+    // MOUSE ENTER MOUSE LEAVE SUPPORT FROM https://gist.github.com/shawnbot/4166283
+    // get a reference to the d3.selection prototype,
+    // and keep a reference to the old d3.selection.on
+    var d3_selectionPrototype = d3.selection.prototype,
+        d3_on = d3_selectionPrototype.on;
+
+    // our shims are organized by event:
+    // "desired-event": ["shimmed-event", wrapperFunction]
+    var shims = {
+    "mouseenter": ["mouseover", relatedTarget],
+    "mouseleave": ["mouseout", relatedTarget]
+    };
+
+    // rewrite the d3.selection.on function to shim the events with wrapped
+    // callbacks
+    d3_selectionPrototype.on = function(evt, callback, useCapture) {
+    var bits = evt.split("."),
+        type = bits.shift(),
+        shim = shims[type];
+    if (shim) {
+        evt = [shim[0], bits].join(".");
+        callback = shim[1].call(null, callback);
+        return d3_on.call(this, evt, callback, useCapture);
+    } else {
+        return d3_on.apply(this, arguments);
+    } 
+    };
+
+    function relatedTarget(callback) {
+    return function() {
+        var related = d3.event.relatedTarget;
+        if (this === related || childOf(this, related)) {
+            return undefined;
+        }
+        return callback.apply(this, arguments);
+    };
+    }
+
+    function childOf(p, c) {
+        if (p === c) return false;
+        while (c && c !== p) c = c.parentNode;
+        return c === p;
+    }
 });
