a5dfe3a414e9c284250fcbedf10aff2c9f4dec49
David Xu <xudavidb@gmail.com>
2013-04-05 20:44:32 -0400

    changed gridview back to normal sizes per row

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,26 +1,19 @@
 
 grid_init = function(){
-  
-
   // GENERATES GRID
-    // backbone globals setup
 
+  $(function(){
 
-    $(function(){
+    app.applications = new app.collections.Applications(APP_DATA);
+    // append the grid to body
+    $('#visualizations').prepend(new app.views.GridView({collection: app.applications}).render().el);
+    $('#more-apps-box').prepend(new app.views.GridView({collection: app.applications}).render().el);
 
-      app.applications = new app.collections.Applications(APP_DATA);
-      // append the grid to body
-      $('#visualizations').prepend(new app.views.GridView({collection: app.applications}).render().el);
-      $('#more-apps-box').prepend(new app.views.GridView({collection: app.applications}).render().el);
-
-      // sample socket
-      // var socket = io.connect(window.location.hostname);
-      // socket.on('news', function (data) {
-      //   console.log(data);
-      //   socket.emit('my other event', { my: 'data' });
-      // });
-    });
-
-
-
+    // sample socket
+    // var socket = io.connect(window.location.hostname);
+    // socket.on('news', function (data) {
+    //   console.log(data);
+    //   socket.emit('my other event', { my: 'data' });
+    // });
+  });
 };
