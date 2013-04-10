4ceb2d4b68ea7c628df0d06d21a8baf3ee3704c5
Gianni Chen <giannikchen@gmail.com>
2013-04-10 15:49:21 -0400

    converting managelist to spaces instead of tabs

--- public/javascripts/managelist.js
+++ public/javascripts/managelist.js
@@ -2,34 +2,34 @@ var allowEndpoint = "/users/whitelist_add";
 var disallowEndpoint = "/users/whitelist_remove";
 
 $(document).ready(function() {
-	$("#allow-button").click(function() {
-		makePost(allowEndpoint);
-	});
+    $("#allow-button").click(function() {
+        makePost(allowEndpoint);
+    });
 
-	$("#disallow-button").click(function() {
-		makePost(disallowEndpoint);
-	});
+    $("#disallow-button").click(function() {
+        makePost(disallowEndpoint);
+    });
 });
 
 function makePost(endpoint) {
-	var domain = $("#input-appname").val();
-	console.log(domain);
-	$.post(endpoint, {"domain": domain}, renderResponse);
+    var domain = $("#input-appname").val();
+    console.log(domain);
+    $.post(endpoint, {"domain": domain}, renderResponse);
 }
 
 function renderResponse(data, status, xhr) {
-	if ("success" in data) {
-		//$("#response").html("Success! " + data["success"]);
-		console.log(data["success"]);
-		var newList = data["success"]["new_whitelist"];
-		var outputHtml = "";
-		for (var i = 0; i < newList.length; i ++) {
-			outputHtml += "<li>" + newList[i] + "</li>";
-		}
-		$("#whitelist").html(outputHtml);
-	}
-	else if ("error" in data) {
-		//$("#response").html("Could not add " + domain + " to apps list");
-		alert(data["error"]);
-	}
+    if ("success" in data) {
+        //$("#response").html("Success! " + data["success"]);
+        console.log(data["success"]);
+        var newList = data["success"]["new_whitelist"];
+        var outputHtml = "";
+        for (var i = 0; i < newList.length; i ++) {
+            outputHtml += "<li>" + newList[i] + "</li>";
+        }
+        $("#whitelist").html(outputHtml);
+    }
+    else if ("error" in data) {
+        //$("#response").html("Could not add " + domain + " to apps list");
+        alert(data["error"]);
+    }
 };
