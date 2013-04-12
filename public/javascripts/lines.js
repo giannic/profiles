2512cc6020edacb1bcd44a33017a900c57eb2206
gaby <morengab@gmail.com>
2013-04-11 20:00:44 -0400

    Made slider handle bars slightly thicker

--- public/javascripts/lines.js
+++ public/javascripts/lines.js
@@ -92,53 +92,52 @@ function myFunction2(x){
   hide_stats();
 }
 
-  function createAllTheHovers(){
-            var hovers = d3.selectAll("line"); // this should change
-            //console.log("hovers = ");
-            //console.log(hovers[0]);
-            for(var i = 0; i < hovers[0].length; i++){
-              currline = hovers[0][i];
-              currline.addEventListener("mouseover",function(evt) { myFunction(this); }, false);
-              currline.addEventListener("mouseout",function(evt) { myFunction2(this); }, false);
-              }
-
+function createAllTheHovers(){
+  var hovers = d3.selectAll("line"); // this should change
+  //console.log("hovers = ");
+  //console.log(hovers[0]);
+  for(var i = 0; i < hovers[0].length; i++){
+    currline = hovers[0][i];
+    currline.addEventListener("mouseover",function(evt) { myFunction(this); }, false);
+    currline.addEventListener("mouseout",function(evt) { myFunction2(this); }, false);
+    }
 }
 
-  function initFreqLine() {
-    
-    var w = lineGraphWidth+24, h = 25;
+function initFreqLine() {
+  
+  var w = lineGraphWidth+24, h = 25;
 
-    for (var i=minRange; i < maxRange-1; i++) {
-        frequencies[i] = 0;
-        calcFreq(i);
-    }
-
-    var graph = d3.select(".frequency-container")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
-
-    var x = d3.scale.linear()
-      .domain([0, 100])
-      .range([0, w]);
-
-    var y = d3.scale.linear()
-      .domain([0, 5]) // this should be the max of the frequencies?
-      .range([h, 0]);
-
-    var line = d3.svg.line()
-        .x(function(d,i) {
-          return x(i);
-        })
-        .y(function(d) {
-          return y(d);
-        })
-
-    var data = frequencies;
-    graph.append("svg:path")
-        .attr("d", line(data))
-        .attr("class", "frequency-line");
+  for (var i=minRange; i < maxRange-1; i++) {
+      frequencies[i] = 0;
+      calcFreq(i);
   }
+
+  var graph = d3.select(".frequency-container")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  var x = d3.scale.linear()
+    .domain([0, 100])
+    .range([0, w]);
+
+  var y = d3.scale.linear()
+    .domain([0, 5]) // this should be the max of the frequencies?
+    .range([h, 0]);
+
+  var line = d3.svg.line()
+      .x(function(d,i) {
+        return x(i);
+      })
+      .y(function(d) {
+        return y(d);
+      })
+
+  var data = frequencies;
+  graph.append("svg:path")
+      .attr("d", line(data))
+      .attr("class", "frequency-line");
+}
 
   function updateFreqLine(data) {
     // TODO
