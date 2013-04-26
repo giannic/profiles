f7b2092e0269769c177e67548146d33a08603548
David Xu <xudavidb@gmail.com>
2013-04-26 19:56:11 -0400

    durations lines working

--- public/javascripts/durations.js
+++ public/javascripts/durations.js
@@ -3,6 +3,7 @@
    * PLEASE LOOK IN CONFIG FILE FOR CONSTANTS FOR OFFSETS
    */
 
+
   var sample_data = [
     {
       focus: [10, 30, 50, 70],
@@ -17,7 +18,7 @@
       unfocus: [10]
     },
     {
-      focus: [70],
+      focus: [80],
       unfocus: [100]
     }
   ];
@@ -26,19 +27,45 @@
   var canvas,
       canvas_ctx,
       canvas_height,
-      start_time = 0,
+      start_time = 0, // change to startTime, endTime
       end_time = 100,
       total_time,
       timeline_width = WINDOW_WIDTH,
       timeline_height = WINDOW_HEIGHT;
+  var ordered_apps;
 
+
+
+  function initialize() {
+    canvas = $('#durations-canvas');
+    canvas = document.getElementById('durations-canvas');
+    canvas_height = WINDOW_HEIGHT - $('#header').height(); // subtract size of menubar
+    //canvas.height = canvas_height;
+    // this is how clusters is doing it atm
+    canvas.height = WINDOW_HEIGHT - 50;
+    canvas.width = WINDOW_WIDTH - 30;
+
+    total_time = end_time - start_time;
+    canvas_ctx = canvas.getContext('2d');
+  }
+
+  /*
+   * main setup function for durations
+   * called inside config.js
+   */
+  app.util.vis.durations_init = function() {
+    initialize();
+    //render_app_segment(300, 40, 60);
+    render_all_apps_from_json(sample_data);
+
+  };
   
   /*
-   * Sums the total duration of an app
+   * order apps by duration 
    * Input: All JSON Data
    * Output: Array sorted in order of most used to least used
    */
-  function sum_all_durations(json) {
+  function order_apps(json) {
       var d_json = _.map(json, sum_duration_per_app, function(memo, dur){ return memo + dur; }); 
       return _.sortBy(d_json, function(app) {
         return -app.durations;
@@ -78,17 +105,6 @@
     var start_x = focus_time / total_time * timeline_width;
     var end_x = unfocus_time / total_time * timeline_width;
 
-
-    /*
-    canvas_ctx.beginPath();
-    canvas_ctx.moveTo(10,10);
-    canvas_ctx.lineTo(10,200);
-    canvas_ctx.lineWidth = 1;
-    canvas_ctx.strokeStyle = "black";
-    canvas_ctx.stroke();
-    canvas_ctx.closePath();
-    */
-
     canvas_ctx.beginPath();
     canvas_ctx.moveTo(start_x, y);
     canvas_ctx.lineTo(end_x, y);
@@ -98,51 +114,27 @@
     canvas_ctx.closePath();
   }
 
-  /* Renders all the line segments for app
-   * Input: app_index, all_focus_data
-   * Output: Draws all the line segments of an app, as well as its icon
-   */
-  function render_app(app_index, focus) {
-
-  }
-
   /* Render all lines for all apps
    * Input: All JSON Data
    * Output: Draws paints entire canvas
    */
-  function render_all_apps() {
-      $.each(APP_DATA.apps, function(idx, app) {
-          sum_duration_per_app(app);
-          // just testing
-          //console.log(app.open[0]);
-          //console.log(app.close[0]);
-      });
+  function render_all_apps_from_json(data) {
+    ordered_apps = order_apps(data);
+    _.each(ordered_apps, function(item, index){
+      render_app(item, index);  
+    });
   }
 
-
-  function initialize() {
-    canvas = $('#durations-canvas');
-    canvas = document.getElementById('durations-canvas');
-    canvas_height = WINDOW_HEIGHT - $('#header').height(); // subtract size of menubar
-    //canvas.height = canvas_height;
-    // this is how clusters is doing it atm
-    canvas.height = WINDOW_HEIGHT - 50;
-    canvas.width = WINDOW_WIDTH - 30;
-
-    total_time = end_time - start_time;
-    canvas_ctx = canvas.getContext('2d');
-  }
-
-  /*
-   * main setup function for durations
-   * called inside config.js
+  /* Renders all the line segments for app
+   * Input: the json data 
+   * Output: Draws all the line segments of an app, as well as its icon
    */
-  app.util.vis.durations_init = function() {
-    initialize();
-    console.log(sum_all_durations(sample_data));
+  function render_app(data, index) {
+    var focus_pairs = _.zip(data.focus, data.unfocus);
+    _.each(focus_pairs, function(pair){
+      render_app_segment(10 * (index + 1), pair[0], pair[1]);
+    });
+  }
 
-    //render_app_segment(300, 40, 60);
-    render_all_apps();
 
-  };
 })();
